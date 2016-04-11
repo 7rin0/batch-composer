c678b007d651b3ce97593a62b67f1ae4423039f1
Luis SEVERINO <lfs.severino@gmail.com>
2016-04-12 01:39:48 +0200

    split composer install to avoid timeouts and memory limits; implement sleep and more installs per execution

---
+++ batchcomposer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Import python modules
+import json
+import subprocess
+from pprint import pprint
+
+# Parse composer.json
+with open('composer.json') as data_file:    
+    data = json.load(data_file)
+
+# For each dict type item require dependency allong their version
+for dependency, version in data['require'].iteritems():
+	requiredDependency = "composer require " + dependency + ":" + version
+	print requiredDependency	
+	subprocess.Popen(requiredDependency, stdout=subprocess.PIPE, shell=True)
