c69337a959ac296501c733148796f8c30811ea85
Luis SEVERINO <lfs.severino@gmail.com>
2016-04-12 01:47:29 +0200

    import and apply time module

--- batch-composer.py
+++ batch-composer.py
@@ -3,6 +3,7 @@
 # Import python modules
 import json
 import subprocess
+import time
 from pprint import pprint
 
 # Parse composer.json
@@ -14,3 +15,4 @@ for dependency, version in data['require'].iteritems():
 	requiredDependency = "composer require " + dependency + ":" + version
 	print requiredDependency	
 	subprocess.Popen(requiredDependency, stdout=subprocess.PIPE, shell=True)
+	time.sleep(60)
