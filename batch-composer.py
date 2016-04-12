65a7852416dc09c4e653d285abce30255b95a054
Luis SEVERINO <lfs.severino@gmail.com>
2016-04-12 03:16:15 +0200

    ok

--- batch-composer.py
+++ batch-composer.py
@@ -7,29 +7,32 @@ import shutil
 import subprocess
 from pprint import pprint
 
+# Switch directories of both composers
+shutil.move('composer.json', 'composer-tmp/composer.json')
+shutil.move('composer-tmp/composer-schema.json', 'composer.json')
+
+# Regenerate composer-schema.json
+open('composer-tmp/composer-schema.json', 'a').close()
+
 # Get real composer.json
-with open('composer.json') as data_file:    
+with open('composer-tmp/composer.json') as data_file:
     current_composer = json.load(data_file)
 
 # This only makes sense if exist key require in dictionary
 # meaning if our composer has packages to install else skip everything
 if 'require' in current_composer:
 	# Clone from real composer.json and save an other empty in required packages
-	tmp_composer = current_composer
-	del tmp_composer['require']
-	with open('composer-tmp/composer-schema.json', 'w') as no_data_file:
-		json.dump(tmp_composer, no_data_file)
-	
-	# Switch directories of both composers
-	shutil.move('composer.json', 'composer-tmp/composer.json')
-	shutil.move('composer-tmp/composer-schema.json', 'composer.json')
-	
-	# For each item dictionary require dependency by defined version
-	#for dependency, version in data['require'].iteritems():
-	#	requiredDependency = "composer require " + dependency + ":" + version
-	#	print requiredDependency	
-	#	subprocess.Popen(requiredDependency, stdout=subprocess.PIPE, shell=True)
-	#	time.sleep(60)
-	
-# Regenerate composer-schema.json
-open('composer-tmp/composer-schema.json', 'a').close()
+    tmp_composer = dict(current_composer)
+    del tmp_composer['require']
+    print current_composer['require']
+    with open('composer.json', 'w') as no_data_file:
+    	json.dump(tmp_composer, no_data_file)
+
+    # For each item dictionary require dependency by defined version
+    for dependency, version in current_composer['require'].iteritems():
+    	requiredDependency = "composer require " + dependency + ":" + version
+    	subprocess.Popen(requiredDependency, stdout=subprocess.PIPE, shell=True)
+    	time.sleep(600)
+
+# Revert action
+shutil.move('composer-tmp/composer.json', 'composer.json')
